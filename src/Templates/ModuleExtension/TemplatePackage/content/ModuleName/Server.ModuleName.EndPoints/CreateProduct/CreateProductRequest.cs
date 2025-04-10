5e1e421503c0d727aebf39432046bbb92c7b0318
RickDev3000 <81363560+SrRickGrimes@users.noreply.github.com>
2025-04-10 14:40:13 +0900

    removed projects

--- src/Templates/ModuleExtension/TemplatePackage/content/ModuleName/Server.ModuleName.EndPoints/CreateProduct/CreateProductRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Server.ModuleName.EndPoints.CreateProduct;
-
-public class CreateProductRequest
-{
-    public required string Name { get; set; }
-    public string? Description { get; set; }
-    public decimal Price { get; set; }
-    public int StockQuantity { get; set; }
-}
--- src/Templates/ModuleExtension/TemplatePackage/content/ModuleName/Server.ModuleName.Infrastructure/Configuration/ModuleNameConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Server.ModuleName.Infrastructure.Configuration;
-
-internal class ModuleNameConfiguration
-{
-    public required string Name { get; set; }
-
-    public string? Description { get; set; }
-}
