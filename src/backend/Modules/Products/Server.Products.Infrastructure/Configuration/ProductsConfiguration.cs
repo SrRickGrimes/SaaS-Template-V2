5069a499f76068f542c9e2e6139606da5b4aaf00
RickDev3000 <81363560+SrRickGrimes@users.noreply.github.com>
2025-04-10 14:22:39 +0900

    added json file for module

---
+++ src/backend/Modules/Products/Server.Products.Infrastructure/Configuration/ProductsConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Products.Infrastructure.Configuration;
+
+internal class ProductsConfiguration
+{
+    public required string ModuleName { get; set; }
+
+    public string? Description { get; set; }
+}
--- src/backend/Modules/Products/Server.Products.Module/ModuleInstaller.cs
+++ src/backend/Modules/Products/Server.Products.Module/ModuleInstaller.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Server.Module.Framework;
 using Server.Products.Infrastructure;
+using Server.Products.Infrastructure.Configuration;
 
 namespace Server.Products.Module;
 
@@ -12,6 +13,17 @@ public class ModuleInstaller : IModuleInstaller
 
     public void Install(IServiceCollection services, IConfiguration configuration)
     {
+        var directory = Path.GetDirectoryName(typeof(ModuleInstaller).Assembly.Location);
+
+        ArgumentException.ThrowIfNullOrEmpty(directory, nameof(directory));
+
+        var currentConfiguration = new ConfigurationBuilder()
+            .SetBasePath(directory)
+            .AddJsonFile("products.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        services.Configure<ProductsConfiguration>(currentConfiguration.GetSection(nameof(Products)));
+
         services.AddInfrastructure();
     }
 
